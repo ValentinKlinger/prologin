cba785c59545729b89538c92b58f0e8be381ee64
valentin <valentink.fr@gmail.com>
2023-11-14 18:03:18 +0100

    My solution for ouroborsos problem.

---
+++ qualification_2024/ouroboros.py
@@ -0,0 +1,130 @@
+class Cellule:
+	def __init__(self, v, s):
+		self.valeur = v
+		self.suivante = s
+    
+class Liste:
+	def __init__(self):
+		self.tete = None
+	def est_vide(self):
+		return self.tete is None
+	def ajoute(self, x):
+		self.tete = Cellule(x, self.tete)
+	def cracher(self):
+		if self.tete is not None:
+			ville = self.tete.valeur
+			self.tete = self.tete.suivante
+			return ville
+		
+
+
+def situation_finale(n: int, m: int, villes: list[str], actions: list[str]):
+    """
+    :param n: le nombre de villes autour de Midgard
+    :param m: le nombre d'années avant le Ragnarök
+    :param villes: le nom des villes autour de Midgard, en partant de la queue de Jörmungandr
+
+    :param actions: la liste des actions prochaines de Jörmungandr
+
+    >>> situation_finale(4, 2, ['Rostheim', 'Adaheim','Pascalheim','Fortrheim'], ['A', 'A'])
+    ['Pascalheim', 'Fortrheim', 'Rostheim', 'Adaheim']
+    >>> situation_finale(4, 4, ['Rostheim', 'Adaheim','Pascalheim','Fortrheim'], ['A', 'M', 'A', 'C'])
+    ['Adaheim', 'Fortrheim', 'Rostheim', 'Pascalheim']
+    >>> situation_finale(4, 1, ['Rostheim', 'Adaheim','Pascalheim','Fortrheim'], ['R'])
+    ['Fortrheim', 'Pascalheim', 'Adaheim', 'Rostheim']
+    >>> situation_finale(4, 5, ['Rostheim', 'Adaheim','Pascalheim','Fortrheim'], ['A', 'M', 'R', 'A', 'C'])
+    ['Adaheim', 'Fortrheim', 'Pascalheim', 'Rostheim']
+    >>> situation_finale(6, 6, 
+    ... ['Nixheim', 'Haskelheim', 'Cobolheim', 'Prologheim', 'Delpheim', 'Modulheim'], ['M', 'M', 'A', 'A', 'C', 'C'])
+    ['Nixheim', 'Haskelheim', 'Delpheim', 'Modulheim', 'Cobolheim', 'Prologheim']
+    >>> situation_finale(2, 2, ['Lispheim', 'Erlangheim'], ['R', 'M'])
+    ['Lispheim']
+    >>> situation_finale(2, 3, ['Lispheim', 'Erlangheim'], ['R', 'M', 'M'])
+    []
+    >>> situation_finale(2, 2, ['A', 'B'], ['R', 'C'])
+    ['B', 'A']
+    >>> situation_finale(2, 4, ['A', 'B'], ['R', 'R', 'R', 'R'])
+    ['A', 'B']
+    >>> situation_finale(3, 9, ['A', 'B', 'C'], ['R', 'A', 'A', 'A', 'A', 'M', 'R', 'C', 'R'])
+    ['A', 'C', 'B']
+    >>> situation_finale(3, 6, ['A', 'B', 'C'], ['R', 'A', 'A', 'A', 'A', 'M'])
+    ['A', 'C']
+    >>> situation_finale(3, 3, ['A', 'B', 'C'], ['R', 'A', 'M'])
+    ['A', 'C']
+    >>> situation_finale(1, 3, ['A'], ['M', 'C', 'C'])
+    ['A']
+    >>> situation_finale(1, 3, ['A'], ['M', 'M', 'C'])
+    ['A']
+    >>> situation_finale(6, 4, ['A', 'B', 'C', 'D', 'E', 'F'], 
+    ... ['A', 'A', 'R', 'M'])
+    ['A', 'F', 'E', 'D', 'C']
+    >>> situation_finale(6, _, ['A', 'B', 'C', 'D', 'E', 'F'], 
+    ... ['A', 'A', 'R', 'M', 'A', 'A', 'A', 'M'])
+    ['C', 'A', 'F', 'E']
+    >>> situation_finale(6, _, ['A', 'B', 'C', 'D', 'E', 'F'], 
+    ... ['A', 'A', 'R', 'M', 'A', 'A', 'A', 'M', 'R', 'A', 'A'])
+    ['A', 'C', 'E', 'F']
+    >>> situation_finale(6, _, ['A', 'B', 'C', 'D', 'E', 'F'], 
+    ... ['A', 'A', 'R', 'M', 'A', 'A', 'A', 'M', 'R', 'A', 'A', 'C'])
+    ['D', 'A', 'C', 'E', 'F']
+    >>> situation_finale(6, 16, ['A', 'B', 'C', 'D', 'E', 'F'], 
+    ... ['A', 'A', 'R', 'M', 'A', 'A', 'A', 'M', 'R', 'A', 'A', 'C', 'R', 'A', 'A', 'C'])
+    ['B', 'C', 'A', 'D', 'F', 'E']
+    >>> situation_finale(6, 16, ['A', 'B', 'C', 'D', 'E', 'F'],
+    ... ['A', 'A', 'R', 'M', 'M', 'R', 'A', 'A', 'R', 'C', 'R', 'A', 'A', 'A', 'A', 'C'])
+    ['B', 'A', 'E', 'F', 'C', 'D']
+    >>> situation_finale(4, 17, ['A', 'B', 'C', 'D'],
+    ... ['R', 'A', 'A', 'A', 'A', 'A', 'M', 'R', 'A', 'A', 'A', 'A', 'A', 'C', 'R', 'A', 'R'])
+    ['A', 'C', 'B', 'D']
+    >>> situation_finale(6, 7, ['A', 'B', 'C', 'D', 'E', 'F'],
+    ... ['C', 'C', 'R', 'M', 'A', 'R', 'C'])
+    ['F', 'E', 'A', 'B', 'C', 'D']
+    """
+    rang_prochaine_ville = 0
+    pas = 1
+    ventre = Liste()
+    for action in actions:
+        if action == 'A':
+            rang_prochaine_ville += pas
+        elif action == 'M' and n != 0:
+            rang_prochaine_ville = rang_prochaine_ville%n
+            ventre.ajoute(villes[rang_prochaine_ville])
+            villes.pop(rang_prochaine_ville)
+            n -= 1
+            if pas == -1:
+                rang_prochaine_ville = rang_prochaine_ville-1
+        elif action == 'R':
+            pas = - pas
+            rang_prochaine_ville += pas
+        elif action == 'C' and ventre.est_vide() is False:
+            if n == 0:
+                villes = [ventre.cracher()]
+                n = 1
+                rang_prochaine_ville = 0
+            elif pas == 1:
+                rang_prochaine_ville = rang_prochaine_ville%n
+                villes.insert(rang_prochaine_ville, ventre.cracher())
+                n += 1
+            elif pas == -1:
+                rang_prochaine_ville += 1
+                rang_prochaine_ville = rang_prochaine_ville%n
+                villes.insert(rang_prochaine_ville%n, ventre.cracher())
+                n += 1
+    output = []
+    try:
+        rang_prochaine_ville = rang_prochaine_ville%n
+    except ZeroDivisionError:
+          return []
+    for _ in range(n):
+        output.append(villes[rang_prochaine_ville%n])
+        rang_prochaine_ville += pas
+    return output
+
+
+if __name__ == "__main__":
+    n = int(input())
+    m = int(input())
+    villes = [input() for _ in range(n)]
+    actions = list(input())
+    for ville in situation_finale(n, m, villes, actions):
+        print(ville)
